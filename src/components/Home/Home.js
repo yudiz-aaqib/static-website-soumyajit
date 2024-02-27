240f510aba6717ef052a6981220b134b5b9c3f4b
yudiz-aaqib <aaqib.shaikh@yudiz.com>
2024-02-27 07:01:06 +0000

    working fine

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -14,38 +14,15 @@ function Home() {
           <Row>
             <Col md={7} className="home-header">
               <h1 style={{ paddingBottom: 15 }} className="heading">
-                Hi There!!!!!!!{" "}
+                Hi There!{" "}
                 <span className="wave" role="img" aria-labelledby="wave">
                   👋🏻
                 </span>
               </h1>
 
               <h1 className="heading-name">
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-                I am
-                <strong className="main-name"> Aaqib</strong>
-=======
-                I'M
-<<<<<<< HEAD
-<<<<<<< HEAD
-                <strong className="main-name"> YUDIZZZZZZZZZZZZZZZZZZZZZZZZZZ</strong>
-=======
-                <strong className="main-name"> SOUMYAJIT</strong>
->>>>>>> parent of 9df4b4c (fresh-add-yudiz)
-=======
-                <strong className="main-name"> SOUMYAJIT</strong>
->>>>>>> parent of 9df4b4c (fresh-add-yudiz)
->>>>>>> parent of ddb695f (working)
-=======
                 I'M
                 <strong className="main-name"> SOUMYAJIT BEHERA</strong>
->>>>>>> parent of 39016dd (aaqib name add)
-=======
-                I'M
-                <strong className="main-name"> SOUMYAJIT BEHERA</strong>
->>>>>>> parent of 39016dd (aaqib name add)
               </h1>
 
               <div style={{ padding: 50, textAlign: "left" }}>
