9df4b4cfeb3b5be4e731761bbc043c1b7281e7d9
Ubuntu <ubuntu@ip-172-31-95-156.ec2.internal>
2024-02-27 06:40:50 +0000

    fresh-add-yudiz

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -22,7 +22,7 @@ function Home() {
 
               <h1 className="heading-name">
                 I'M
-                <strong className="main-name"> SOUMYAJIT</strong>
+                <strong className="main-name"> YUDIZ</strong>
               </h1>
 
               <div style={{ padding: 50, textAlign: "left" }}>
